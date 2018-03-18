bd2f11a1e29e47cd33e5623bbe23382a18231bc5
elcgithub <37487676+elcgithub@users.noreply.github.com>
2018-03-18 08:07:41 -0700

    Update content.js

--- content.js
+++ content.js
@@ -9,59 +9,19 @@ var images = [
 'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/premium-weave.jpg)',
 'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/ss058.jpg)',
 'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/rubber-ducks-final.jpg)'
+'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/ComingWithClouds11.jpg)'
+'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/burnieani.gif)'
+'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/water002.gif)'
 ];
 
 var fonts = [
 	'Times New Roman',
 	'Impact',
-	'Comic Sans MS',
-	'Hobo Std',
-	'Verdana'
-];
-
-var strings =[
-	"Abcde",
-	"Zyxwv",
-	"Welcome to my homepage!",
-	"You've got mail",
-	"Space Jam",
-	"Welcome to Netscape",
-	"Untitled",
-	"Just Monika",
-	"Je suis une pizza",
-	"Rawr XD",
-	"Welcome to my twisted mindddddd",
-	"Hello darkness, my old friend",
-	"java.lang.NullPointerException",
-	"windows95tips.com",
-	"cabbi.bo",
-	"how bout i do anyway",
-	"It's a secret to everybody",
-	"Omae wa mou shindeiru",
-	"Nyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanyanya",
-	"This webpage is unavailable",
-	"Never gonna give you up",
-	"Smash that like button and subscribe for more videos",
-	"Quality content since 1996",
-	"No smoking",
-	"No parking between 8am-6pm",
-	"War is peace. Freedom is slavery. Ignorance is strength.",
-	"INSUFFICIENT DATA FOR MEANINGFUL ANSWER",
-	"the girl who was no longer blind saw 'the tree with the lights in it.'",
-	"In death, a man or a woman is free of the weight of the past",
-	"The Past in Death; That which we cannot escape.",
-	"endorphin has performed an illegal operation",
-	"Windows has encountered an problem and needs to close.",
-	"Nene papa wain choudai",
-	"Your breath fades away",
-	"Click here to download more RAM",
-	"Itsumo hitori de aruiteta",
-	"how is babby formed"
+	'Comic Sans MS'
 ];
 
 var randimage = images[Math.floor(Math.random() * images.length)];
 var randfont = fonts[Math.floor(Math.random() * fonts.length)];
-var randstring = strings[Math.floor(Math.random() * strings.length)];
 
 var x = document.querySelectorAll('p,li,h1,h2,h3,h4,h5,h6,div,body,input');
 var i;
@@ -82,7 +42,7 @@ var sounds = [
 ];
 
 var marq = document.createElement("marquee");
-var text = document.createTextNode(randstring);
+var text = document.createTextNode("Welcome To My Website!");
 marq.appendChild(text);
 document.body.appendChild(marq);
 
