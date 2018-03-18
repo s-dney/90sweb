df85130120fa4c4fe16da1b7d3cbc4d88392984e
AshZhang <ashley.w.zhang@gmail.com>
2018-03-17 22:50:52 -0700

    Fixed image marquee so it no longer blocks links

--- content.js
+++ content.js
@@ -8,7 +8,6 @@ var images = [
 'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/back_mac.gif)',
 'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/premium-weave.jpg)',
 'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/ss058.jpg)',
-
 'url(https://raw.githubusercontent.com/s-dney/90sweb/master/backgrounds/rubber-ducks-final.jpg)'
 ];
 
@@ -19,18 +18,14 @@ var fonts = [
 
 var randimage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundColor = "blue";
-
-
-document.div.style.backgroundImage = randimage;
-document.p.style.backgroundImage = randimage;
-document.body.style.backgroundImage = randimage;
-
+// document.body.style.backgroundColor = "blue";
+// document.div.style.backgroundImage = randimage;
+// document.p.style.backgroundImage = randimage;
+// document.body.style.backgroundImage = randimage;
 
 var marq = document.createElement("marquee");
 var text = document.createTextNode("Welcome To My Website!");
 marq.appendChild(text);
-marq.id = "marquee";
 document.body.appendChild(marq);
 
 var marqimg = document.createElement("marquee");
@@ -43,14 +38,23 @@ marqimg.appendChild(img);
 marqimg.position = "relative";
 marqimg.zIndex = 999;
 marqimg.position = "fixed";
+
 marqimg.behavior = "alternate";
 marqimg.left = 30;
 marqimg.scrollamount = "50";
 marqimg.direction = "down";
 
-marqimg.width = screen.width;
+
 img.onload = function(){
 	imgHeight = img.height;
 	marqimg.height = screen.height - imgHeight;
+	marqimg.width = img.width;
 	document.body.appendChild(marqimg);
+}
+
+var sound = document.createElement("audio");
+sound.src = "http://www.thesoundarchive.com/play-wav-files.asp?sound=email/youGotmail.mp3";
+sound.loop = true;
+sound.onload = function(){
+	sound.play();
 }
